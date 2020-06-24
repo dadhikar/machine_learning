936bf601db15570bd0d947d23238d6d004a281ba
Dasharath Adhikari <dadhikar@dadhikar.local>
2020-06-24 15:09:58 -0400

    1.10

---
+++ Iris_data_classification_#1/performance_on_two_features/logistic_rg_iris_data_classification.py
@@ -0,0 +1,107 @@
+# author: Dasharath Adhikari
+import os
+import sys
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['xtick.top'] = True
+mpl.rcParams['ytick.right'] = True
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+import plot_decision_boundary as pdb
+
+current_dir = Path.cwd()
+print('current_directory:\n', current_dir)
+
+
+data_file_path = os.path.join(current_dir, "data")
+print('data_file_directory:\n', data_file_path)
+
+for file in os.listdir(data_file_path):
+    print('-'*10)
+    print(file)
+
+
+# Information about the  Iris data
+# Number of Instances: 150 (50 in each of three classes)
+iris_data = {0: 'sepal length', 1: 'sepal width', 2: 'petal length',
+             3: 'petal width'}
+class_label = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
+
+df = pd.read_csv(data_file_path + os.sep + "iris.data",  skiprows=0, header=None)
+# target variable
+y = df.iloc[:, 4].map(class_label).values
+# print(df.head())
+# sys.exit()
+# feature matrix
+print('*'*30)
+first_feature = int(input('Please enter first feature >> '))
+second_feature = int(input('Please enter second feature >> '))
+print('*'*30)
+X = df.iloc[:, [first_feature, second_feature]]
+# standardization of the feature matrix
+std_sc = StandardScaler(copy=True, with_mean=True, with_std=True)
+# X_new = std_sc.fit_transform(X)
+# Compute the mean and std to be used for later scaling
+std_sc.fit(X)
+# Perform standardization by centering and scaling and return X
+X_std = std_sc.transform(X)  # standardized feature matrix
+
+# random splitting of train and test data
+# splitting date for training and test
+X_train, X_test, y_train, y_test = train_test_split(X_std, y, train_size=0.8,
+                                                    random_state=1,
+                                                    shuffle=True,  stratify=y)
+
+# Logistic regression
+# C, Inverse of regularization strength; must be a positive float
+lr = LogisticRegression(penalty='l2', dual=False, tol=0.0001, C=100,
+                        fit_intercept=True, intercept_scaling=1,
+                        class_weight=None, random_state=None, solver='lbfgs',
+                        max_iter=100, multi_class='auto', verbose=0,
+                        warm_start=False, n_jobs=None, l1_ratio=None)
+
+# fit the model according to the given training data
+lr.fit(X_train, y_train, sample_weight=None)
+# print(lr.coef_[0])
+# print(lr.intercept_)
+# print(lr.n_iter_)
+# sys.exit()
+# predict class labels for samples in X.
+predict_class = lr.predict(X_test)
+# predicting classification accuracy
+list1 = []
+for pr, test in zip(predict_class, y_test):
+    if pr != test:
+        list1.append(pr)
+predict_accuracy = 1 - len(list1)/len(y_test)
+print(r'Prediction accuracy is:{}'.format(predict_accuracy))
+
+# predict logarithm of probability estimates.
+# lr.predict_log_proba(X_test[:5, :])
+# lr.predict_proba(X_test)
+
+id = {0: 'sepal length [ab. unit]', 1: 'sepal width [ab. unit]',
+      2: 'petal length [ab. unit]', 3: 'petal width [ab. unit]'}
+
+# plotting the decision boundary
+
+
+pdb.decision_boundary(X_std, y, X_test, y_test, classifier=lr)
+# print('*'*30)
+# x = input('Please enter first feature positional index >> ')
+# y = input('Please enter second feature positional index >> ')
+# print('*'*30)
+plt.xlabel(id[first_feature], fontsize=15)
+plt.ylabel(id[second_feature], fontsize=15)
+plt.legend()
+plt.title(r"Logistic Regression accuracy: {} %".
+          format(round(predict_accuracy*100, 2)), fontsize=15)
+plt.show()
---
+++ Iris_data_classification_#1/performance_on_two_features/plot_decision_boundary.py
@@ -0,0 +1,38 @@
+"""Created on Tue May  5 15:37:55 2020 @author: dadhikar"""
+
+import os
+import numpy as np
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['xtick.direction'] = 'in'
+mpl.rcParams['ytick.direction'] = 'in'
+mpl.rcParams['xtick.top'] = True
+mpl.rcParams['ytick.right'] = True
+
+
+def decision_boundary(X, y, X_test, y_test, classifier):
+    """
+    """
+    x1_min, x1_max = X[:, 0].min()-1, X[:, 0].max()+1
+    x2_min, x2_max = X[:, 1].min()-1, X[:, 1].max()+1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.01),
+                           np.arange(x2_min, x2_max, 0.01))
+    x1_grid = xx1.ravel()
+    x2_grid = xx2.ravel()
+    # x1x2_grid contains each possible combination of x and y value
+    x1x2_grid = np.array([x1_grid, x2_grid]).T
+    y_predict = classifier.predict(x1x2_grid)
+    y_predict = y_predict.reshape(xx1.shape)
+    # plotting the decision boundary
+    colors = ['magenta', 'skyblue', 'seagreen', 'C3', 'C4', 'C5']
+    markers = ["o", "v", "H", "*", "h"]
+    cmap = ListedColormap(colors[:len(np.unique(y))], N=None)
+    plt.contourf(xx1, xx2, y_predict, cmap=cmap, alpha=0.5)
+    # plotting test samples
+    for idx, cl in enumerate(np.unique(y_test)):
+        plt.scatter(x=X_test[y_test == cl, 0], y=X_test[y_test == cl, 1],
+                    c=colors[idx], marker=markers[idx], s=100, alpha=1,
+                    edgecolor='b', label=cl)
+
+# print(os.getcwd())
